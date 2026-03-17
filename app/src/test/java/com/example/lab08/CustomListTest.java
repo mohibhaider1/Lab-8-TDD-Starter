54a3a8bee215215a0988cff19742739ee587d72e
mohibhaider1 <mohibhaider090@gmail.com>
2026-03-18 03:40:03 +0500

    1st test

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -8,7 +8,7 @@ public class CustomListTest {
         CustomList list = new CustomList();
         City calgary = new City("Calgary", "AB");
         list.addCity(calgary);
-        assertTrue(list.hasCity(calgary));  // fails — hasCity() doesn't exist yet
+        assertTrue(list.hasCity(calgary));
     }
 
 
